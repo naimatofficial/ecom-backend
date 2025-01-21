b9fe6bbd01349014a360a206b8b1364cffd8cc66
naimatofficial <naimat.official.10@gmail.com>
2025-01-21 16:28:52 +0500

    update index

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import axios from 'axios'
 
 import adminRoutes from './admin/index.js'
 import userRoutes from './users/index.js'
@@ -9,15 +10,46 @@ import sellerRoutes from './sellers/index.js'
 
 import { validateSessionToken } from '../middleware/authMiddleware.js'
 import { httpSearchProducts } from '../controllers/search.js'
+import catchAsync from '../utils/catchAsync.js'
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     res.status(200).json({
         status: 'success',
-        message: '🛒 Vista Mart API is running successfully',
+        message: '🛒 API is running successfully',
     })
 })
+
+router.post(
+    '/location',
+    catchAsync(async (req, res) => {
+        const { latitude, longitude } = req.body
+
+        // Reverse geocode the coordinates to get city and country
+        const { data: geoData } = await axios.get(
+            `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
+        )
+        // const geoData = await geoResponse
+
+        const { data: countries } = await axios.get(
+            'https://restcountries.com/v3.1/all'
+        )
+
+        const country = countries.find(
+            (country) => country.cca2 === geoData.countryCode
+        )
+        const { flags } = country // Extract the flags property
+
+        res.status(200).json({
+            status: 'success',
+            city: geoData.city,
+            country: geoData.countryName,
+            code: geoData.countryCode,
+            flags,
+        })
+    })
+)
 
 router.post('/validate-session', validateSessionToken)
 
